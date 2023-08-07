969b6ed6ec8a1b2ffee1a9cdd62396d674f46e73
Boanerges <basicboanerges@gmail.com>
2023-08-07 13:48:57 -0600

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#define GL_SILENCE_DEPRECATION
+#include <GLFW/glfw3.h>
+#include <OpenGL/gl.h>
+#include <SDL2/SDL.h>
+
+// copied directly from https://www.glfw.org/documentation.html
+// with exception to #include <SDL2/SDL.h>, which was used for testing
+// and #define GL_SILENCE_DEPRECATION to suppress opengl depretiation warnings
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
